e7ae7b592a5a7274b602a7419c974242aece8157
InsectMk <makun_ing@126.com>
2025-04-15 17:26:32 +0800

    feat(每日饭菜Admin): 增加获取食材精简信息列表的接口

--- yudao-module-meals/yudao-module-meals-biz/src/main/java/cn/iocoder/yudao/module/meals/controller/app/food/AppFoodController.java
+++ yudao-module-meals/yudao-module-meals-biz/src/main/java/cn/iocoder/yudao/module/meals/controller/app/food/AppFoodController.java
@@ -2,7 +2,9 @@ package cn.iocoder.yudao.module.meals.controller.app.food;
 
 import cn.iocoder.yudao.framework.common.pojo.CommonResult;
 import cn.iocoder.yudao.framework.common.util.object.BeanUtils;
+import cn.iocoder.yudao.module.meals.controller.admin.food.vo.FoodSimpleRespVO;
 import cn.iocoder.yudao.module.meals.controller.app.food.vo.AppFoodRespVO;
+import cn.iocoder.yudao.module.meals.convert.food.FoodConvert;
 import cn.iocoder.yudao.module.meals.dal.dataobject.food.FoodDO;
 import cn.iocoder.yudao.module.meals.service.food.FoodService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 
@@ -31,5 +35,13 @@ public class AppFoodController {
     public CommonResult<AppFoodRespVO> getFood(@RequestParam("id") Long id) {
         FoodDO food = foodService.getFood(id);
         return success(BeanUtils.toBean(food, AppFoodRespVO.class));
+    }
+
+    @GetMapping("/list-all-simple")
+    @Operation(summary = "获取食材精简信息列表", description = "主要用于前端的下拉选项")
+    public CommonResult<List<FoodSimpleRespVO>> getSimpleFoodList() {
+        // 获用户列表，只要开启状态的
+        List<FoodDO> list = foodService.getFoodList();
+        return success(FoodConvert.INSTANCE.convertSimpleList(list));
     }
 }
