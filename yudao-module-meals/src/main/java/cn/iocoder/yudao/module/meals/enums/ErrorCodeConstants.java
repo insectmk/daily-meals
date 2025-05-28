7acced97bdc8e2c6b4e30963483cb1519751059e
InsectMk <makun_ing@126.com>
2025-05-28 15:24:52 +0800

    chore(每日饭菜): 增加评论异常代码

--- yudao-module-meals/src/main/java/cn/iocoder/yudao/module/meals/enums/ErrorCodeConstants.java
+++ yudao-module-meals/src/main/java/cn/iocoder/yudao/module/meals/enums/ErrorCodeConstants.java
@@ -51,4 +51,7 @@ public interface ErrorCodeConstants {
     // ========== 用户收藏 ==========
     ErrorCode USER_FAVOR_NOT_EXISTS = new ErrorCode(200_011_001, "用户收藏不存在");
     ErrorCode USER_FAVOR_ALREADY_CANCEL = new ErrorCode(200_011_002, "已取消收藏");
+
+    // ========== 评论 ==========
+    ErrorCode COMMENT_NOT_EXISTS = new ErrorCode(200_012_001, "评论不存在");
 }
