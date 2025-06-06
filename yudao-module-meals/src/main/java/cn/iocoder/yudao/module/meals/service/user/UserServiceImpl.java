4806c8cbeae818379687533b07a161b6f22cc13b
InsectMk <makun_ing@126.com>
2025-06-06 19:12:36 +0800

    chore(每日饭菜App): 完成获取用户的关注数、粉丝数、获赞与收藏量的接口

--- yudao-module-meals/src/main/java/cn/iocoder/yudao/module/meals/service/user/UserServiceImpl.java
+++ yudao-module-meals/src/main/java/cn/iocoder/yudao/module/meals/service/user/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package cn.iocoder.yudao.module.meals.service.user;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.map.MapUtil;
 import cn.iocoder.yudao.framework.common.util.object.BeanUtils;
 import cn.iocoder.yudao.module.meals.controller.app.user.vo.AppUserInfoRespVO;
 import cn.iocoder.yudao.module.meals.controller.app.user.vo.AppUserInteractDataRespVO;
 import cn.iocoder.yudao.module.meals.convert.user.UserConvert;
+import cn.iocoder.yudao.module.meals.dal.dataobject.userfavor.UserFavorDO;
 import cn.iocoder.yudao.module.meals.dal.mysql.userfavor.UserFavorMapper;
 import cn.iocoder.yudao.module.meals.enums.ContentTypesEnum;
+import cn.iocoder.yudao.module.meals.enums.RecipeTypesEnum;
 import cn.iocoder.yudao.module.member.api.user.MemberUserApi;
 import cn.iocoder.yudao.module.member.api.user.dto.MemberUserRespDTO;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -36,15 +41,41 @@ public class UserServiceImpl implements UserService {
     @Override
     public AppUserInteractDataRespVO getUserInteractData(String userId) {
         AppUserInteractDataRespVO result = new AppUserInteractDataRespVO();
-        // todo 需要完善
         // 关注
-        result.setFollows(178L);
+        List<Map<String, Object>> followsMaps = userFavorMapper.selectMaps(new QueryWrapper<UserFavorDO>()
+                .select("count(distinct content_id) as follows")
+                .eq("content_type", ContentTypesEnum.USER.getType()) // 用户类型
+                .eq("user_id", userId) // 该用户的
+        );
+        if (CollUtil.isEmpty(followsMaps)) {
+            result.setFollows(0L);
+        } else {
+            result.setFollows(MapUtil.getLong(followsMaps.get(0), "follows", 0L));
+        }
         // 粉丝
-        result.setFans(123L);
+        List<Map<String, Object>> fansMaps = userFavorMapper.selectMaps(new QueryWrapper<UserFavorDO>()
+                .select("count(distinct user_id) as fans")
+                .eq("content_type", ContentTypesEnum.USER.getType()) // 用户类型
+                .eq("content_id", userId) // 关注为该用户的
+        );
+        if (CollUtil.isEmpty(fansMaps)) {
+            result.setFans(0L);
+        } else {
+            result.setFans(MapUtil.getLong(fansMaps.get(0), "fans", 0L));
+        }
         // 点赞
-        result.setLikes(267L);
-        // 收藏
-        result.setCollects(2738L);
+        result.setLikes(0L);
+        // 被收藏
+        List<Map<String, Object>> collectMaps = userFavorMapper.selectMaps(new QueryWrapper<UserFavorDO>()
+                .select("count(distinct content_id,user_id) as collects")
+                .eq("content_type", ContentTypesEnum.RECIPE.getType()) // 菜谱类型
+                .apply(String.format("exists(select 1 from meals_recipe r where r.user_id = '%s' and recipe_type = %d  and r.id = meals_user_favor.content_id and deleted = 0)", userId, RecipeTypesEnum.USER.getType())) // 收藏为该用户的
+        );
+        if (CollUtil.isEmpty(collectMaps)) {
+            result.setCollects(0L);
+        } else {
+            result.setCollects(MapUtil.getLong(collectMaps.get(0), "collects", 0L));
+        }
         return result;
     }
 
